903812ebda439faaea324fbb438e7458681628a1
Viktor Basanko <v.basanko@outlook.com>
2023-03-08 15:21:30 +0200

    Created User and Auth Modules and connect to mongo

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import {getMongoConfig} from "./configs/mongo.config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true,  envFilePath: 'envs/.account.env'}),
+    UserModule,
+    AuthModule,
+    MongooseModule.forRootAsync(getMongoConfig())],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
